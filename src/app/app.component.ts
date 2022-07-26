0d8e98829e8f8b22a624cb8683069d86fa171448
sag128 <sagarmot6@gmail.com>
2022-07-26 11:18:12 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'practice-management';
+
+  formGroup!: FormGroup;
+  titleAlert: string = 'This field is required';
+  post: any = '';
+  form: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm();
+    // this.setChangeValidate()
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(10)]],
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      dob: [null, [Validators.required]],
+      address: [null],
+      country: [null],
+      gender: [null]
+    });
+
+
+  }
+
+  createForm() {
+    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    this.formGroup = this.formBuilder.group({
+      'email': [null, [Validators.required, Validators.pattern(emailregex)]],
+      'name': [null, Validators.required],
+      'password': [null, [Validators.required]],
+      'description': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+      'validate': ''
+    });
+  }
+
+
+
+
+  saveDetails(form: any) {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(form.value, null, 4));
+  }
 }
+
